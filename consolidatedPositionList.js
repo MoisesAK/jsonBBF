6e2b2030eed1427ef19c7186fe402bb013b23f82
Moisés Alcides Kaufmann <41123375+MoisesAK@users.noreply.github.com>
2019-07-24 14:13:37 -0300

    Create consolidatedPositionList.js

---
+++ consolidatedPositionList.js
@@ -0,0 +1,56 @@
+[
+        {
+            codigo: "1",
+            descripcion: "Cuentas Corrientes y de ahorro"
+        },
+        {
+            codigo: "2",
+            descripcion: "Tarjetas de Crédito"
+        },
+        {
+            codigo: "3",
+            descripcion: "Préstamos"
+        },
+        {
+            codigo: "4",
+            descripcion: "Depósitos a plazo fijo"
+        },
+        {
+            codigo: "5",
+            descripcion: "Leasing"
+        },
+        {
+            codigo: "6",
+            descripcion: "Factoring"
+        },
+        {
+            codigo: "7",
+            descripcion: "Catas de Fianza"
+        },
+        {
+            codigo: "8",
+            descripcion: "Cobranzas internacionales"
+        },
+        {
+            codigo: "9",
+            descripcion: "Cobranzas"
+         
+        },
+        {
+            codigo: "10",
+            descripcion: "Descuentos"
+        },
+        {
+            codigo: "11",
+            descripcion: "Línea de Crédito de letras"
+        },
+        {
+            codigo: "12",
+            descripcion: ""
+        },
+        {
+            codigo: "13",
+            descripcion: ""
+        }
+
+]
